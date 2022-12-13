544b865664afaa426302cef812946119bc03bd8e
lizhanhui <lizhanhui@gmail.com>
2022-12-14 00:22:09 +0800

    BoxFuture

--- src/hello_world.rs
+++ src/hello_world.rs
@@ -1,6 +1,6 @@
 use std::task::{Context, Poll};
 
-use futures::future::{ready, Ready};
+use futures::future::{ready, BoxFuture, Ready};
 
 use hyper::{http, Body, Request, Response};
 use log::trace;
@@ -12,7 +12,7 @@ impl Service<Request<Body>> for HelloWorld {
     type Response = Response<Body>;
     type Error = http::Error;
 
-    type Future = Ready<Result<Self::Response, Self::Error>>;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
 
     fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         Poll::Ready(Ok(()))
@@ -20,11 +20,17 @@ impl Service<Request<Body>> for HelloWorld {
 
     fn call(&mut self, req: Request<Body>) -> Self::Future {
         let method = req.method().clone();
-        let path = req.uri().path();
+        let path = req.uri().path().to_owned();
         trace!("Received a request: {} - {}", method, path);
-        // create the body
-        let body = Body::from("foo, bar, baz!\n");
-        let response = Response::new(body);
-        ready(Ok(response))
+        // Caveat: memory allocation for each request. It's too expensive.
+        Box::pin(async move {
+            // create the body
+            let body = Body::from("foo, bar, baz!\n");
+            let response = Response::new(body);
+            let resp = ready(Ok(response)).await;
+            // In Rust, Futures are lazy!!
+            trace!("Finished processing requset: {} - {}", method, path);
+            resp
+        })
     }
 }
